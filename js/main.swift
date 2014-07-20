7a40c98b12a47d761f2fd628c1921c9f3ad13d68
Dan Kogai <dankogai+github@gmail.com>
2014-07-20 11:59:58 +0900

    renamed .(get|set) to .(fetch|store)

--- js/main.swift
+++ js/main.swift
@@ -21,34 +21,35 @@ jsv = ctx.evaluateScript("this")
 println(jsv)
 
 extension JSContext {
-    func get(key:NSString) -> JSValue {
+    func fetch(key:NSString)->JSValue {
         return getJSVinJSC(self, key)
     }
-    func set(key:NSString, _ val:AnyObject) {
+    func store(key:NSString, _ val:AnyObject) {
         setJSVinJSC(self, key, val)
     }
-    func set(key:NSString, _ blk:(()->AnyObject!)?) {
+    func store(key:NSString, _ blk:()->AnyObject!) {
         setB0JSVinJSC(self, key, blk)
     }
-    func set(key:NSString, _ blk:((AnyObject!)->AnyObject!)?) {
+    func store(key:NSString, _ blk:(AnyObject!)->AnyObject!) {
         setB1JSVinJSC(self, key, blk)
     }
-    func set(key:NSString, _ blk:((AnyObject!,AnyObject!)->AnyObject!)?) {
+    func store(key:NSString,
+        _ blk:(AnyObject!,AnyObject!)->AnyObject!) {
         setB2JSVinJSC(self, key, blk)
     }
 }
-ctx.set("ary", [1,2,3])
-jsv = ctx.evaluateScript("bar = foo.map(function(n){return n*n})")
-println(ctx.get("bar"))
+ctx.store("ary", [1,2,3])
+jsv = ctx.evaluateScript("bar=foo.map(function(n){return n*n})")
+println(ctx.fetch("bar"))
 
 // block w/ no argument
-ctx.set("hello") { ()->AnyObject! in
+ctx.store("hello") { ()->AnyObject! in
     return "Hello, JS! I am Swift."
 }
 println(ctx.evaluateScript("hello"))
 println(ctx.evaluateScript("hello()"))
 // block w/ 1 argument
-ctx.set("square") { (o:AnyObject!)->AnyObject! in
+ctx.store("square") { (o:AnyObject!)->AnyObject! in
     if let x = o as? Double {
         return x * x
     }
@@ -58,7 +59,7 @@ println(ctx.evaluateScript("square"))
 println(ctx.evaluateScript("square()"))
 println(ctx.evaluateScript("square(6)"))
 // block w/ 2 arguments
-ctx.set("multiply") { (o:AnyObject!, p:AnyObject!)->AnyObject! in
+ctx.store("multiply") { (o:AnyObject!, p:AnyObject!)->AnyObject! in
     if let x = o as? Double {
         if let y = p as? Double {
             return x * y
@@ -71,7 +72,7 @@ println(ctx.evaluateScript("multiply()"))
 println(ctx.evaluateScript("multiply(6)"))
 println(ctx.evaluateScript("multiply(6,7)"))
 // for any more arguments, just use array instead
-ctx.set("sum") { (o:AnyObject!)->AnyObject! in
+ctx.store("sum") { (o:AnyObject!)->AnyObject! in
     if let a = o as? NSArray {
         var result = 0.0
         for v in a {
